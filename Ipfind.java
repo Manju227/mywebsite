5eec9b80cf29701466d00658bf4b7cc38a4711b4
Manjunath Katageri <manjunath59039@gmail.com>
2026-07-06 09:55:12 +0530

    Implement IP address lookup for a given URL
    Add functionality to retrieve IP address from a website URL.

--- Ipfind.java
+++ Ipfind.java
@@ -1,1 +1,27 @@
+import java.net.*;
+import java.util.Scanner;
+public class Ipfind {
+public static void main(String[] args) {
+Scanner s= new Scanner(System.in);
+System.out.print("Enter the website URL: ");
+String host= s.nextLine();
+try {
+InetAddress inetAddress=InetAddress.getByName(host);
+String ip=inetAddress.getHostAddress();
+System.out.println("IP Address of " + host+" is: "+ ip);
+} catch  (UnknownHostException e) {
+
+System.out.println("Unable to resolve " + host);
+}
+s.close();
+}
+}
+
+
+
+
+
+
+
+
 
